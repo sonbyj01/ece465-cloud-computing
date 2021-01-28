222a4fa0c882f6ad8b8af52414a3c748ddc9c7eb
Jonathan Lam <jlam55555@gmail.com>
2021-01-27 22:34:36 -0500

    made fields public

--- src/proj1/graph/generate_graphs.go
+++ src/proj1/graph/generate_graphs.go
@@ -1,27 +1,54 @@
 package graph
 
-// CompleteGraph generates a complete graph with nodeCount nodes
-func CompleteGraph(nodeCount int) Graph {
-	graph := Graph{make([]Node, nodeCount)}
+import (
+	"math/rand"
+)
 
-	for i := 0; i < nodeCount; i++ {
+// CompleteGraph generates a complete graph with nodeCount nodes
+func NewCompleteGraph(nodeCount int) Graph {
+	g := New(nodeCount)
+
+	for i := 1; i < nodeCount; i++ {
 		for j := 0; j < i; j++ {
-			graph.AddUndirectedEdge(i, j)
+			g.AddUndirectedEdge(i, j)
 		}
 	}
 
-	return graph
+	return g
 }
 
 // RingGraph generates a graph in which each node has exactly two neighbors
-func RingGraph(nodeCount int) Graph {
-	// TODO: implement this
-	return Graph{}
+func NewRingGraph(nodeCount int) Graph {
+	g := New(nodeCount)
+
+	for i := 0; i < nodeCount-1; i++ {
+		g.AddUndirectedEdge(i, i+1)
+	}
+
+	// if n=2, this edge is already created
+	if nodeCount > 2 {
+		g.AddUndirectedEdge(0, nodeCount-1)
+	}
+
+	return g
 }
 
 // RandomGraph generates a graph with nodeCount nodes and average
 // branching factor bFactor
-func RandomGraph(nodeCount int, bFactor float32) Graph {
-	// TODO: implement this
-	return Graph{}
+func NewRandomGraph(nodeCount int, bFactor float32) Graph {
+	g := New(nodeCount)
+
+	// if branching factor is bFactor, then given n1, n2 nodes in g, then the
+	// probability of an undirected edge is bFactor / (nodeCount - 1)
+	pEdge := bFactor / float32(nodeCount-1)
+
+	for i := 1; i < nodeCount; i++ {
+		for j := 0; j < i; j++ {
+			if rand.Float32() < pEdge {
+				g.AddUndirectedEdge(i, j)
+			}
+		}
+	}
+
+	return g
 }
--- src/proj1/graph/graph.go
+++ src/proj1/graph/graph.go
@@ -1,34 +1,44 @@
 package graph
 
-// Node represents a node of a Graph object
+// Node represents a node of a Graph object, with an adjacency list
+// of pointers to other nodes
 type Node struct {
-	value int
-	adj []*Node
+	Value int
+	Adj   []*Node
 }
 
-// Graph represents a generic graph data structure
+// Graph represents a very simple graph data structure
 type Graph struct {
-	nodes []Node
+	Nodes []Node
+}
+
+// New returns a new graph
+// Since this is namespaced under the graph package, can be used from the
+// outside as graph.New(...)
+func New(nodeCount int) Graph {
+	return Graph{make([]Node, nodeCount)}
 }
 
 // AddNode adds a node to a graph
 func (g *Graph) AddNode(value int) {
-	g.nodes = append(g.nodes, Node{value, make([]*Node, 0)})
+	g.Nodes = append(g.Nodes, Node{value, make([]*Node, 0)})
 }
 
 // AddUndirectedEdge adds an undirected edge between two nodes in a graph
+// Note that this doesn't check for duplicate edges
 func (g *Graph) AddUndirectedEdge(n1, n2 int) {
-	if n1 >= len(g.nodes) || n2 >= len(g.nodes) {
+	if n1 >= len(g.Nodes) || n2 >= len(g.Nodes) {
 		panic("Invalid node indices")
 	}
 
-	g.nodes[n1].adj = append(g.nodes[n1].adj, &g.nodes[n2])
-	g.nodes[n2].adj = append(g.nodes[n2].adj, &g.nodes[n1])
+	g.Nodes[n1].Adj = append(g.Nodes[n1].Adj, &g.Nodes[n2])
+	g.Nodes[n2].Adj = append(g.Nodes[n2].Adj, &g.Nodes[n1])
 }
 
 // AddUndirectedEdge adds an undirected edge to another node pointer
-// note that this doesn't check if the second node is within the same graph
+// Note that this doesn't check if the second node is within the same graph,
+// and this doesn't check for duplicate edges
 func (n1 *Node) AddUndirectedEdge(n2 *Node) {
-	n2.adj = append(n2.adj, n1)
-	n1.adj = append(n1.adj, n2)
+	n2.Adj = append(n2.Adj, n1)
+	n1.Adj = append(n1.Adj, n2)
 }
