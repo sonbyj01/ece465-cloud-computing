72eab1f27c02bfa65068706b867873b0eaccde6b
Jonathan Lam <jlam55555@gmail.com>
2021-01-27 20:42:28 -0500

    changed adjacency list to slice of node pointers

--- src/proj1/graph/graph.go
+++ src/proj1/graph/graph.go
@@ -3,7 +3,7 @@ package graph
 // Node represents a node of a Graph object
 type Node struct {
 	value int
-	edges []int
+	edges []*Node
 }
 
 // Graph represents a generic graph data structure
@@ -13,11 +13,22 @@ type Graph struct {
 
 // AddNode adds a node to a graph
 func (g *Graph) AddNode(value int) {
-	g.nodes = append(g.nodes, Node{value, make([]int, 0)})
+	g.nodes = append(g.nodes, Node{value, make([]*Node, 0)})
 }
 
 // AddUndirectedEdge adds an undirected edge between two nodes in a graph
 func (g *Graph) AddUndirectedEdge(n1, n2 int) {
-	g.nodes[n1].edges = append(g.nodes[n1].edges, n2)
-	g.nodes[n2].edges = append(g.nodes[n2].edges, n1)
+	if n1 >= len(g.nodes) || n2 >= len(g.nodes) {
+		panic("Invalid node indices")
+	}
+
+	g.nodes[n1].edges = append(g.nodes[n1].edges, &g.nodes[n2])
+	g.nodes[n2].edges = append(g.nodes[n2].edges, &g.nodes[n1])
+}
+
+// AddUndirectedEdge adds an undirected edge to another node pointer
+// note that this doesn't check if the second node is within the same graph
+func (n1 *Node) AddUndirectedEdge(n2 *Node) {
+	n2.edges = append(n2.edges, n1)
+	n1.edges = append(n1.edges, n2)
 }
