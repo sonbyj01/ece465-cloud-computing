d7ca71fa37108587595f0a3e6873484933697db9
BY Son <sonbyj01@gmail.com>
2021-02-10 22:45:28 -0500

    wrote node/connect.go

--- src/graphnet/node.go
+++ src/graphnet/node.go
@@ -1,12 +1,37 @@
 // Package graphnet includes the network datastructures and utility functions
 // for the multi-node algorithm
+
+// Server
+
 package graphnet
 
 import "net"
 
 type Node struct {
-	ip     net.IPAddr
+	ip     net.IP
 	port   int
+}
 
-	// TODO: socket
+func (node *Node) InitializeNode(portP int) {
+	// Sets the default port value to 8000
+	if portP == 0 && node.port == 0 {
+		node.port = 8000
+	}
+
+	// Loops through all the available interfaces on the machine 
+	// And assigns the IP address and default port to the Node Struct
+	ifaces, _ := net.Interfaces()
+
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				node.ip = v.IP
+			case *net.IPAddr:
+				node.ip = v.IP
+			}
+		}
+	}
 }
